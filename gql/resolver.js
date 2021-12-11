9f709e7d821c23f6d0d3eeb0481f4d92c7b837f2
Matias Dominguez Alonso <matiasdominguez3@gmail.com>
2021-12-11 16:23:43 -0300

    end first step of mutation function user

--- gql/resolver.js
+++ gql/resolver.js
@@ -1,3 +1,5 @@
+const User = require("../models/user");
+
 const resolvers = {
   Query: {
     // User
@@ -9,9 +11,31 @@ const resolvers = {
 
   Mutation: {
     // User
-    register: (_, { input }) => {
-      console.log("Registrando usuarios");
-      return null;
+    register: async (_, { input }) => {
+      const newUser = input;
+      newUser.email = newUser.email.toLowerCase();
+      newUser.username = newUser.username.toLowerCase();
+
+      const { email, username, password } = newUser;
+
+      // Check email
+      const foundEmail = await User.findOne({ email });
+      if (foundEmail) throw new Error("Email already in use");
+
+      // Check username
+      const foundUserName = await User.findOne({ username });
+      if (foundUserName) throw new Error("Username already in use");
+
+      // Encrypt
+      // ....
+
+      try {
+        const user = new User(newUser);
+        user.save();
+        return user;
+      } catch {
+        console.log(error);
+      }
     },
   },
 };
